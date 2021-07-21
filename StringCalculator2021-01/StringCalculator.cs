605861cd3da12221ef97a9b098398d3f60be6f71
Houndonougbo G. Vincent <houndvince@gmail.com>
2021-07-21 17:12:40 +0000

    step 5 done

--- StringCalculator2021-01/StringCalculator.cs
+++ StringCalculator2021-01/StringCalculator.cs
@@ -23,10 +23,20 @@ namespace StringCalculator2021_01
                 delimiters.Add(Convert.ToChar(newDelimiter));
             }
 
-            var result = numberString.Split(delimiters
+            var numberList = numberString.Split(delimiters
                 .ToArray())
-                .Select(s => int.Parse(s))
-                .Sum();
+                .Select(s => int.Parse(s));
+
+            var negatives = numberList.Where(n => n < 0);
+
+            if (negatives.Any())
+            {
+                string negativeString = String.Join(',', negatives
+                    .Select(n => n.ToString()));
+                throw new Exception($"Negatives not allowed: {negativeString}");
+            }
+
+            var result = numberList.Sum();
 
             return result;
         }
--- StringCalculator2021-01/StringCalculator_Add.cs
+++ StringCalculator2021-01/StringCalculator_Add.cs
@@ -1,3 +1,4 @@
+using System;
 using Xunit;
 
 namespace StringCalculator2021_01
@@ -62,12 +63,26 @@ namespace StringCalculator2021_01
 
         [Theory]
         [InlineData("//;\n1;2;3", 6)]
-        public void ReturnsSumGivenStringWithCustomDelimiter( 
+        public void ReturnsSumGivenStringWithCustomDelimiter(
             string numbers, int expectedResult)
         {
             var result = _calculator.Add(numbers);
 
             Assert.Equal(expectedResult, result);
         }
+
+        [Theory]
+        [InlineData("-1,2", "Negatives not allowed: -1")]
+        [InlineData("-1,-2", "Negatives not allowed: -1,-2")]
+        public void ThrowsGivenNegativeInputs(
+            string numbers, string expectedMessage)
+        {
+            Action action = () => _calculator.Add(numbers);
+
+            var ex = Assert.Throws<Exception>(action);
+
+            Assert.Equal(expectedMessage, ex.Message);
+        }
+
     }
 }
